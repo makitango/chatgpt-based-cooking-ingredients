e95d91d4ba42131bdf95c625f08efbe743b64568
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-20 12:48:27 +0200

    all changes lol

--- src/main/java/com/example/chatgptbasedcookingingredients/ChatgptBasedCookingIngredientsApplication.java
+++ src/main/java/com/example/chatgptbasedcookingingredients/ChatgptBasedCookingIngredientsApplication.java
@@ -1,10 +1,28 @@
 package com.example.chatgptbasedcookingingredients;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ChatgptBasedCookingIngredientsApplication {
+
+    @Value("${app.openai-api-key}")
+    private String openaiApiKey;
+
+    @Value("${app.openai-org}")
+    private String openaiOrg;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.openai.com/v1/chat/completions")
+                .defaultHeader("Authorization", "Bearer " + openaiApiKey)
+                .defaultHeader("OpenAI-Organization", openaiOrg)
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChatgptBasedCookingIngredientsApplication.class, args);
--- src/main/java/com/example/chatgptbasedcookingingredients/IngredientController.java
+++ src/main/java/com/example/chatgptbasedcookingingredients/IngredientController.java
@@ -2,22 +2,74 @@ package com.example.chatgptbasedcookingingredients;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ingredients")
 @RequiredArgsConstructor
 public class IngredientController {
 
+    private final WebClient webClient;
+
     @PostMapping
     String categorizeIngredient(@RequestBody String ingredient) {
+        return webClient.post()
+                .bodyValue(new ChatGPTRequest("Tell me whether " + ingredient + " is vegan, vegetarian or regular. Answer with 'unknown' if you do not have an answer. Only respond with one word."))
+                .retrieve()
+                .bodyToMono(ChatGPTResponse.class)
+                .map(response -> {
+                    String responseText = response.text();
+                    System.out.println(responseText);
+                    if ("vegan".equalsIgnoreCase(responseText)) {
+                        return "vegan";
+                    } else if ("vegetarian".equalsIgnoreCase(responseText)) {
+                        return "vegetarian";
+                    } else if ("regular".equalsIgnoreCase(responseText)) {
+                        return "regular";
+                    } else {
+                        return "unknown";
+                    }
+                }).block();
 
-        // TODO: This should return "vegan", "vegetarian" or "regular" depending on the ingredient.
+    }
 
-        return "vegan";
+    record ChatGPTMessage(
+            String role,
+            String content
+    ) {
+    }
+
+    record ChatGPTRequest(
+            String model,
+            List<ChatGPTMessage> messages
+    ) {
+        ChatGPTRequest(String message) {
+            this("gpt-3.5-turbo", Collections.singletonList(new ChatGPTMessage("user", message)));
+        }
+    }
+
+    record ChatGPTChoice(
+            ChatGPTMessage message
+    ) {
+    }
+
+    record ChatGPTResponse(
+            List<ChatGPTChoice> choices
+    ) {
+        public String text() {
+            return choices.get(0).message().content();
+        }
     }
 
 }
